b4232ae2f577f7ec12a77c77e2776b467b94d717
Luay Bachtiar <luaygitaris@gmail.com>
2025-03-05 13:28:00 +0700

    merubah port pada server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,34 @@
-import 'dotenv/config.js'
+import 'dotenv/config.js';
 import express from 'express';
 import cors from 'cors';
-import {connectDB} from './config/db.js'
+import { connectDB } from './config/db.js';
 import productRouter from './routes/productRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
-
-
 const app = express();
-// const port = 4000;
+const port = 3001;
 
 app.use(express.json());
-app.use(cors(
-	{
-		origin: ["https://vercel.com/luay-bachtiar-rifais-projects/fuisonmart-mern"],
-		methods: ["POST", "GET"],
-		credentials: true
-	}
-));
+app.use(
+  cors({
+    origin: ["https://vercel.com/luay-bachtiar-rifais-projects/fuisonmart-mern"],
+    methods: ["POST", "GET"],
+    credentials: true
+  })
+);
 
-connectDB()
-
-app.use("/api/product", productRouter)
-// app.use("/images", express.static('uploads'))
-app.use("/api/user", userRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
+connectDB();
+app.use("/api/product", productRouter);
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get('/', (req, res) => {
-	res.send('API Working');
+  res.send('API Working');
 });
 
-app.listen(3001, () => {
-	console.log(`Server started on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
 });
