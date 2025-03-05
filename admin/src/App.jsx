a1e74149046e2bc5da6ee6a1706d314e6fd1c06a
Luay Bachtiar <luaygitaris@gmail.com>
2025-03-05 14:14:37 +0700

    merubah typo pad admin di file app.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
-import Add from './pages/add';
+import Add from './pages/Add';
 import List from './pages/List';
 import Orders from './pages/Orders';
 import { ToastContainer } from 'react-toastify';
