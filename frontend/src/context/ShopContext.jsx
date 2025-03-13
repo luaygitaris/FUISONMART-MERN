17d12158f79a88b3d28fbf5a8088667b37effd19
Luay Bachtiar <luaygitaris@gmail.com>
2025-03-13 09:55:32 +0700

    merubah url pada frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -5,7 +5,7 @@ export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
 	const [cartItems, setCartItems] = useState({});
-	const url = 'http://localhost:4000';
+	const url = 'https://fuisonmart-mern-backend.vercel.app/';
 	const [token, setToken] = useState('');
 	const [all_products, setAll_products] = useState([]);
 
