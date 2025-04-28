f082cc6b77dec6ae6c9c2bfc7f8ad722a2f6e5d2
luaygitaris <luaygitaris.com>
2025-04-28 12:51:49 +0700

    merubah backend

--- backend/index.js
+++ backend/index.js
@@ -11,23 +11,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Routes
 app.use('/api/product', productRouter);
 app.use('/api/user', userRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
 
-// Database connection
 mongoose
     .connect(process.env.MONGO_DB)
     .then(() => console.log('Database is connected'))
     .catch((error) => console.error('Database connection error:', error));
 
-// Default route
 app.get('/', (req, res) => {
     res.send('API Working');
 });
 
-// Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4002;
 app.listen(PORT, () => console.log(`Server running on ${PORT} PORT`));
