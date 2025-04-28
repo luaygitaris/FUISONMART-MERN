bf2af73f88c93ad7112b6b6162156ffee6192aac
luaygitaris <luaygitaris.com>
2025-04-28 13:24:29 +0700

    merubah dbjs agar timeout lebih lama

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,20 @@
+// import mongoose from 'mongoose';
+
+// export const connectDB = async () => {
+// 	await mongoose
+// 		.connect(
+// 			'mongodb+srv://luaygitaris:user123@cluster0.3cyau.mongodb.net/FusionMartYT'
+// 		)
+// 		.then(() => console.log('DB Connected'));
+// };
+
 import mongoose from 'mongoose';
 
-export const connectDB = async () => {
-	await mongoose
-		.connect(
-			'mongodb+srv://luaygitaris:user123@cluster0.3cyau.mongodb.net/FusionMartYT'
-		)
-		.then(() => console.log('DB Connected'));
-};
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB error:', err));
